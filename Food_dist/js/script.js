975ae7289de95e5afe208407325987e0fc4c9133
Yevgenii <oliinyk1994@gmail.com>
2021-04-09 14:57:40 +0300

    add XMLHttpRequest for modal

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -197,7 +197,9 @@ window.addEventListener('DOMContentLoaded', () => {
         "img/tabs/vegy.jpg",
         'vegy',
         'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        `Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих 
+         овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно 
+         новый продукт с оптимальной ценой и высоким качеством!`,
         9,
         '.menu .container',
         'menu__item',
@@ -207,7 +209,9 @@ window.addEventListener('DOMContentLoaded', () => {
         "img/tabs/elite.jpg",
         'elite',
         'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        `В меню “Премиум” мы используем не только красивый дизайн упаковки, 
+         но и качественное исполнение блюд. Красная рыба, морепродукты, 
+         фрукты - ресторанное меню без похода в ресторан!`,
         14,
         '.menu .container',
         'menu__item'
@@ -217,7 +221,10 @@ window.addEventListener('DOMContentLoaded', () => {
         "img/tabs/post.jpg",
         'post',
         'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        `Меню “Постное” - это тщательный подбор ингредиентов: 
+         полное отсутствие продуктов животного происхождения, 
+         молоко из миндаля, овса, кокоса или гречки, правильное 
+         количество белков за счет тофу и импортных вегетарианских стейков.`,
         21,
         '.menu .container',
         'menu__item'
@@ -226,11 +233,11 @@ window.addEventListener('DOMContentLoaded', () => {
     // Forms
 
     const forms = document.querySelectorAll('form');
-    
+
     const message = {
-        loading: 'Загрузка...',
-        success: 'Спасибо! Скоро мы с вами свяжемся',
-        failure: 'Что-то пошло не так...'
+        loading: 'Загрузка',
+        success: 'Спасибо! Скоро мы с Вами свяжемся',
+        failure: 'Что-то пошло не так'
     };
 
     forms.forEach(item => {
@@ -238,27 +245,30 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
     function postData(form) {
-        form.addEventListener('submit', (e) => {
+        form.addEventListener('Submit', (e) => {
             e.preventDefault();
 
             const statusMessage = document.createElement('div');
             statusMessage.classList.add('status');
             statusMessage.textContent = message.loading;
             form.append(statusMessage);
-        
+
             const request = new XMLHttpRequest();
             request.open('POST', 'server.php');
-            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+            request.setRequestHeader('Content-type', 'application/json');
             const formData = new FormData(form);
 
             const object = {};
-            formData.forEach(function(value, key){
-                object[key] = value;
+
+            formData.forEach(function(value, key) {
+                object[key]= value;
             });
+
             const json = JSON.stringify(object);
 
             request.send(json);
- 
+            
             request.addEventListener('load', () => {
                 if (request.status === 200) {
                     console.log(request.response);
@@ -271,7 +281,7 @@ window.addEventListener('DOMContentLoaded', () => {
                     statusMessage.textContent = message.failure;
                 }
             });
-        });
+        });  
     }
 
 });
