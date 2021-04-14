4d7b7a0012d365b9f2138c7e5262415437599286
Yevgenii <oliinyk1994@gmail.com>
2021-04-14 22:46:19 +0300

    add dots to slider

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -306,6 +306,7 @@ window.addEventListener('DOMContentLoaded', () => {
     let slideIndex = 1;
 
     const slides = document.querySelectorAll('.offer__slide'),
+        slider = document.querySelector('.offer__slider'),
         prev = document.querySelector('.offer__slider-prev'),
         next = document.querySelector('.offer__slider-next'),
         total = document.querySelector('#total'),
@@ -332,6 +333,50 @@ window.addEventListener('DOMContentLoaded', () => {
         slide.style.width = width;
     });
 
+    slider.style.position = 'relative';
+
+    const indicators = document.createElement('ol'),
+          dots = [];
+    indicators.classList.add('carousel-indicators');
+    indicators.style.cssText = `
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-right: 15%;
+        margin-left: 15%;
+        list-style: none;
+    `; // Если хотите - добавьте в стили, но иногда у нас нет доступа к стилям
+    slider.append(indicators);
+
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li');
+        dot.setAttribute('data-slide-to', i + 1);
+        dot.style.cssText = `
+            box-sizing: content-box;
+            flex: 0 1 auto;
+            width: 30px;
+            height: 6px;
+            margin-right: 3px;
+            margin-left: 3px;
+            cursor: pointer;
+            background-color: #fff;
+            background-clip: padding-box;
+            border-top: 10px solid transparent;
+            border-bottom: 10px solid transparent;
+            opacity: .5;
+            transition: opacity .6s ease;
+        `;
+        if (i == 0) {
+            dot.style.opacity = 1;
+        }
+        indicators.append(dot);
+        dots.push(dot);
+    }
+
     next.addEventListener('click', () => {
         if (offset == (+width.slice(0, width.length - 2) * (slides.length - 1))) {
             offset = 0;
@@ -352,6 +397,9 @@ window.addEventListener('DOMContentLoaded', () => {
         } else {
             current.textContent =  slideIndex;
         }
+
+        dots.forEach(dot => dot.style.opacity = ".5");
+        dots[slideIndex-1].style.opacity = 1;
     });
 
     prev.addEventListener('click', () => {
@@ -374,50 +422,29 @@ window.addEventListener('DOMContentLoaded', () => {
         } else {
             current.textContent =  slideIndex;
         }
+
+        dots.forEach(dot => dot.style.opacity = ".5");
+        dots[slideIndex-1].style.opacity = 1;
     });
 
-    
-    // showSlides(slideIndex);
+    dots.forEach(dot => {
+        dot.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute('data-slide-to');
 
-    // if (slides.length < 10) {
-    //     totalSlides.textContent = `0${slides.length}`;
-    // } else {
-    //     totalSlides.textContent = slides.length;
-    // }
+            slideIndex = slideTo;
+            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
 
-    // function showSlides(n) {
-    //     if (n > slides.length) {
-    //         slideIndex = 1;
-    //     }
+            slidesField.style.transform = `translateX(-${offset}px)`;
 
-    //     if (n < 1) {
-    //         slideIndex = slides.length;
-    //     }
+            if (slides.length < 10) {
+                current.textContent =  `0${slideIndex}`;
+            } else {
+                current.textContent =  slideIndex;
+            }
 
-    //     slides.forEach((item) => {
-    //         item.classList.add('hide');
-    //         item.classList.remove('show');
-    //     });
-
-    //     slides[slideIndex - 1].classList.add('show');
-
-    //     if (slides.length < 10) {
-    //         currentSlide.textContent = `0${slideIndex}`;
-    //     } else {
-    //         currentSlide.textContent = slideIndex;
-    //     }
-    // }
-
-    // function changeSlide(n) {
-    //     showSlides(slideIndex += n);
-    // }
-
-    // prevSlide.addEventListener('click', function() {
-    //     changeSlide(-1);
-    // });
-
-    // nextSlide.addEventListener('click', function() {
-    //     changeSlide(1);
-    // });
+            dots.forEach(dot => dot.style.opacity = ".5");
+            dots[slideIndex-1].style.opacity = 1;
+        });
+    });
 
 });
